f7db2f9e4bc5f9eae46e0e2cfad10ce454377c38
Davel <jonasdavesumbeling@gmail.com>
2025-06-08 14:27:47 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/dave-portfolio/',
+    base: '/portfolio-dave/',
     plugins: [react()],
     build: {
         rollupOptions: {
